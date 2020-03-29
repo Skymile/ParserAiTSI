01e2b5db766b0e1205c6bf5640052a9043b607cd
Patryk Milewski <mpatryk97@gmail.com>
2020-03-29 18:55:13 +0200

    Init!

---
+++ ParserAiTSI/Core/Node.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Core
+{
+	public class Parser
+	{
+		public Parser LoadFile(string filename) =>
+			File.Exists(filename)
+				? LoadScript(File.ReadAllText(filename))
+				: throw new FileNotFoundException($"\"{filename}\" was not found.");
+
+		public Parser LoadScript(string text)
+		{
+
+
+			return this;
+		}
+
+		public Parser Add(Node node)
+		{
+			Node next = this.Root;
+
+			while (next.First != null)
+				next = next.First;
+			next.First = node;
+
+			return this;
+		}
+
+		public readonly Node Root = new Node(Instruction.Statements);
+	}
+
+	public class Node
+	{
+		public Node(Instruction instruction, params Node[] list)
+		{
+			this.InsType = instruction;
+			if (list.Length > 0)
+				this.List = new List<Node>(list);
+		}
+
+		public Node First  { get; set; }
+		public Node Second { get; set; }
+
+		public object Value { get; set; }
+		public string Token { get; set; }
+
+
+		public readonly IEnumerable<Node> List;
+		public readonly Instruction InsType;
+	}
+
+	public enum Instruction : byte
+	{
+		NoOp,
+		Conditional,
+		Assign,
+		Declare,
+		Expression,
+		Loop,
+		Statements,
+		Call,
+		Constant,
+	}
+}
---
+++ ParserAiTSI/Terminal/Program.cs
@@ -0,0 +1,47 @@
+﻿using Core;
+
+namespace Terminal
+{
+	class Program
+	{
+		static void Main()
+		{
+			var parser = new Parser();
+
+			parser.LoadFile("input/1.txt");
+			/*
+				var x = 4;
+				if (x == 2) {
+				    ;
+				}
+			*/
+
+			var root = new Node(Instruction.Statements,
+				new Node(Instruction.Assign)
+				{
+					Token = "x",
+					Value = 4
+				},
+				new Node(Instruction.Conditional)
+				{
+					First = new Node(Instruction.Expression)
+					{
+						Token = "==",
+						First = new Node(Instruction.Call)
+						{
+							Token = "x"
+						},
+						Second = new Node(Instruction.Constant)
+						{
+							Value = 2
+						}
+					},
+					Second = new Node(Instruction.NoOp)
+				}
+			);
+
+			
+
+		}
+	}
+}
