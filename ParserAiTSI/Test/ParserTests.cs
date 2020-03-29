79bbabe40d80eab7bca55b483b343bc5a4930a63
Patryk Milewski <mpatryk97@gmail.com>
2020-03-29 23:26:23 +0200

    Test

---
+++ ParserAiTSI/Test/ParserTests.cs
@@ -0,0 +1,20 @@
+﻿using Core;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+	[TestClass]
+	public class ParserTests
+	{
+		[TestMethod]
+		public void AssignTest()
+		{
+			var parser = new Parser();
+
+			parser.LoadScript("var x = 4;");
+
+			;
+		}
+	}
+}
