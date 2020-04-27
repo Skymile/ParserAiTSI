81ed95f97f2ee4b1a5389ab345beef15c86ac2de
Patryk Milewski <mpatryk97@gmail.com>
2020-04-27 20:15:16 +0200

    Input

--- ParserAiTSI/Terminal/Program.cs
+++ ParserAiTSI/Terminal/Program.cs
@@ -9,42 +9,9 @@ namespace Terminal
 		{
 			var parser = new Parser();
 
-			parser.Load("input/1.txt");
-			/*
-				var x = 4;
-				if (x == 2) {
-				    ;
-				}
-			*/
+			parser.Load("../../input/1.txt");
 
 			Console.ReadLine();
-
-			//var root = new Node(Instruction.Statements,
-			//	new Node(Instruction.Assign)
-			//	{
-			//		Token = "x",
-			//		Value = 4
-			//	},
-			//	new Node(Instruction.Conditional)
-			//	{
-			//		First = new Node(Instruction.Expression)
-			//		{
-			//			Token = "==",
-			//			First = new Node(Instruction.Call)
-			//			{
-			//				Token = "x"
-			//			},
-			//			Second = new Node(Instruction.Constant)
-			//			{
-			//				Value = 2
-			//			}
-			//		},
-			//		Second = new Node(Instruction.NoOp)
-			//	}
-			//);
-
-			
-
 		}
 	}
 }
