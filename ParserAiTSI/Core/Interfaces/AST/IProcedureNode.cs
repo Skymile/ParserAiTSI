04fc8443eb4d3f423db7e9ba611328882216c70e
justyna.dabrowska <justynakamila7@gmail.com>
2020-05-31 17:13:39 +0200

    Calls, Next 2

---
+++ ParserAiTSI/Core/Interfaces/AST/IProcedureNode.cs
@@ -0,0 +1,13 @@
+﻿using Core.Interfaces.PQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces.AST
+{
+    public interface IProcedureNode : INode
+    {
+    }
+}
---
+++ ParserAiTSI/Core/Interfaces/PQL/ICalls.cs
@@ -0,0 +1,20 @@
+﻿using Core.Interfaces.AST;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces.PQL
+{
+    public interface ICalls
+    {
+        void SetCall(IProcedureNode proc1, IProcedureNode proc2);
+
+        IEnumerable<IProcedureNode> GetCalls(IProcedureNode proc);
+
+        IEnumerable<IProcedureNode> GetCalledFrom(IProcedureNode proc);
+
+        bool isCall(IProcedureNode proc1, IProcedureNode proc2);
+    }
+}
