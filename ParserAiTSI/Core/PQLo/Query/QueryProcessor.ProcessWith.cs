fa532fd072b1b82d64f77cbbe1f5a1b7ff0f3ee9
Patryk Milewski <mpatryk97@gmail.com>
2020-06-10 00:02:19 +0200

    File split

---
+++ ParserAiTSI/Core/PQLo/Query/QueryProcessor.ProcessWith.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.PQLo.QueryPreProcessor
+{
+	public partial class QueryProcessor
+	{
+		private IEnumerable<string> ProcessWith(WithData data, Dictionary<string, StatementType> d)
+		{
+			switch (data.Type)
+			{
+				case WithType.Procedure:
+					break;
+				case WithType.Call:
+					break;
+				case WithType.Variable:
+					break;
+				case WithType.Constant:
+					break;
+				case WithType.Stmt:
+					break;
+			}
+			return Enumerable.Empty<string>();
+		}
+	}
+}
